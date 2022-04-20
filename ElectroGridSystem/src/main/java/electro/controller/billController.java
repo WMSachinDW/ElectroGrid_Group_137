1520fb5dd89a36dfb12d28250b9111624d9fee21
Sachin Wijekoon <40454482+WMSachinDW@users.noreply.github.com>
2022-04-20 23:03:55 +0530

    Add files via upload
    Bill Calculation

---
+++ ElectroGridSystem/src/main/java/electro/controller/billController.java
@@ -0,0 +1,102 @@
+package electro.controller;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.*;
+import org.json.simple.*;
+
+import electro.model.*;
+import electro.service.*;
+
+import org.apache.tomcat.util.json.JSONParser;
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/bill")
+public class billController {
+
+	BillService bill_Service =new BillService();
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String view(String app_text)
+	{
+		return bill_Service.getBill();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String add(String app_text)
+	{
+		
+		//Convert the input string to a JSON object
+		JsonObject app = new JsonParser().parse(app_text).getAsJsonObject();
+		
+		Bill bill = new Bill();
+		bill.setUser_id(Integer.parseInt(app.get("user_id").getAsString()));
+		bill.setLast_month_unit_read(Integer.parseInt(app.get("m_unit").getAsString()));
+		bill.setCurrent_unit_read(Integer.parseInt(app.get("c_unit").getAsString()));
+		bill.setUsage(Integer.parseInt(app.get("usage").getAsString()));
+		bill.setDate(app.get("date").getAsString());
+		bill.setTotal(Double.parseDouble(app.get("total").getAsString()));
+		
+		bill_Service.addBill(bill);
+		
+		JSONObject json = new JSONObject();
+		json.put("success", bill_Service.getSuccess());
+		
+		return json.toString();
+			
+		
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String edit(String app_text)
+	{
+		
+		//Convert the input string to a JSON object
+		JsonObject app = new JsonParser().parse(app_text).getAsJsonObject();
+
+		Bill bill = new Bill();
+		bill.setId(Integer.parseInt(app.get("id").getAsString()));
+		bill.setUser_id(Integer.parseInt(app.get("user_id").getAsString()));
+		bill.setLast_month_unit_read(Integer.parseInt(app.get("m_unit").getAsString()));
+		bill.setCurrent_unit_read(Integer.parseInt(app.get("c_unit").getAsString()));
+		bill.setUsage(Integer.parseInt(app.get("usage").getAsString()));
+		bill.setDate(app.get("date").getAsString());
+		bill.setTotal(Double.parseDouble(app.get("total").getAsString()));
+		
+		bill_Service.editBill(bill);
+		
+		JSONObject json = new JSONObject();
+		json.put("success", bill_Service.getSuccess());
+		
+		return json.toString();
+			
+	}
+	
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String delete(String app_text)
+	{
+		
+		//Convert the input string to a JSON object
+		JsonObject app = new JsonParser().parse(app_text).getAsJsonObject();
+		
+		bill_Service.deleteBill(Integer.parseInt(app.get("id").getAsString()));
+		
+		JSONObject json = new JSONObject();
+		json.put("success", bill_Service.getSuccess());
+		
+		return json.toString();
+			
+		
+	}
+	
+}
